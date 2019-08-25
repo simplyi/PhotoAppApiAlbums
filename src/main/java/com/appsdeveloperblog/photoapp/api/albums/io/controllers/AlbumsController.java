6a618c7545e23aa132c40ab1cbeb031eff3b3dc0
Sergey Kargopolov <skargopolov@central1.com>
2019-08-25 07:11:41 -0700

    Dockere file and other minor changes

--- src/main/java/com/appsdeveloperblog/photoapp/api/albums/io/controllers/AlbumsController.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/albums/io/controllers/AlbumsController.java
@@ -19,21 +19,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @RestController
 @RequestMapping("/users/{id}/albums")
 public class AlbumsController {
     
     @Autowired
     AlbumsService albumsService;
-  
+    Logger logger = LoggerFactory.getLogger(this.getClass());
     @GetMapping( 
             produces = { 
                 MediaType.APPLICATION_JSON_VALUE,
                 MediaType.APPLICATION_XML_VALUE,
             })
     public List<AlbumResponseModel> userAlbums(@PathVariable String id) {
-       
+
         List<AlbumResponseModel> returnValue = new ArrayList<>();
         
         List<AlbumEntity> albumsEntities = albumsService.getAlbums(id);
@@ -46,7 +47,7 @@ public class AlbumsController {
         Type listType = new TypeToken<List<AlbumResponseModel>>(){}.getType();
  
         returnValue = new ModelMapper().map(albumsEntities, listType);
- 
+        logger.info("Returning " + returnValue.size() + " albums");
         return returnValue;
     }
 }
